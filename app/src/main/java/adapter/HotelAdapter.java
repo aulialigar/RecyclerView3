842ae1bdc0113e6557788d9f9c28461ae1fd96e7
Smktelkom <aulia_hanani_24rpl@student.smktelkom-mlg.sch.id>
2016-11-06 08:35:32 +0700

    RecyclerView Edit

--- app/src/main/java/adapter/HotelAdapter.java
+++ app/src/main/java/adapter/HotelAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -58,6 +60,10 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
         ImageView tvFoto;
         TextView tvJudul;
         TextView tvDeskripsi;
+        Button bEdit;
+        Button bDelete;
+        ImageButton ibFav;
+        ImageButton ibShare;
 
         public ViewHolder(View itemView)
         {
@@ -65,7 +71,10 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
             tvFoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
             tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
-
+            bEdit = (Button) itemView.findViewById(R.id.buttonEdit);
+            bDelete = (Button) itemView.findViewById(R.id.buttonDelete);
+            ibFav = (ImageButton) itemView.findViewById(R.id.buttonFavorite);
+            ibShare = (ImageButton) itemView.findViewById(R.id.buttonShare);
             itemView.setOnClickListener(new View.OnClickListener()
             {
                 @Override
@@ -74,11 +83,47 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
                     mIHotelAdapter.doClick(getAdapterPosition());
                 }
             });
+            bEdit.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doEdit(getAdapterPosition());
+                }
+            });
+            bDelete.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doDelete(getAdapterPosition());
+                }
+            });
+            ibFav.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doFav(getAdapterPosition());
+                }
+            });
+            ibShare.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doShare(getAdapterPosition());
+                }
+            });
         }
     }
 
     public interface IHotelAdapter
     {
         void doClick(int pos);
+        void doEdit(int pos);
+        void doDelete(int pos);
+        void doFav(int pos);
+        void doShare(int pos);
     }
 }
